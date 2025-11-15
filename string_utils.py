04ec9700abc7fd0371a24b25c8582751a2cd05cb
lihiaviram <lihiviram@gmail.com>
2025-11-15 13:50:47 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -1,17 +1,18 @@
 def split_before_each_uppercases(formula):
     word=""
     index=1
-    new_formula[]=[]
+    new_formula=[]
     if formula[0].isupper():
        word+=formula[0]
-       while index<=len(formula):
+       while index < len(formula) :
              if formula[index].isupper()==False:
                 word+=formula[index]
                 index+=1
-             elif formula[index].isupper(): 
-                  new_formula.append(word)
-                  word=formula[index]
-                  index+=1
+             elif formula[index].isupper():  
+                    new_formula.append(word)
+                    word=formula[index]
+                    index+=1
+
        new_formula.append(word)
        return new_formula
 
