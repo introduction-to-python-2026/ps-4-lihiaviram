d6e444a8b4c8609b2f94289f89c27031d6138170
lihiaviram <lihiviram@gmail.com>
2025-11-15 13:48:57 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -4,7 +4,7 @@ def split_before_each_uppercases(formula):
     new_formula[]=[]
     if formula[0].isupper():
        word+=formula[0]
-       while index<=len(formula) :
+       while index<=len(formula):
              if formula[index].isupper()==False:
                 word+=formula[index]
                 index+=1
@@ -12,16 +12,8 @@ def split_before_each_uppercases(formula):
                   new_formula.append(word)
                   word=formula[index]
                   index+=1
-
-    new_formula.append(word)
-    return new_formula
-                    
-                
-                
-                
-                
-            
-
+       new_formula.append(word)
+       return new_formula
 
 def split_at_first_digit(formula):
     word=""
